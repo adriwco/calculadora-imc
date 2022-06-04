83ab6d1464cc2cc073c96d09ed241bb0b9508cb2
Adriano <80191040+adriwco@users.noreply.github.com>
2022-06-04 13:38:11 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,50 +1,36 @@
-const btnMobile = document.getElementById('btn-mobile');
+onload = () =>{
+  document.getElementById("btn-mobile").onclick = () => toggleMenu();
+  document.getElementById("submit").onclick = () => imc();
+}
+
 function toggleMenu(){
   const nav = document.getElementById('nav');
   nav.classList.toggle('active');
 }
-btnMobile.addEventListener('click', toggleMenu);
-console.log('oito');
-
-const submit = document.getElementById('submit');
 
 function imc(){
+  const select = document.getElementById('sexo');
+  const sexo = select.options[select.selectedIndex];
   const altura = document.getElementById('altura').value;
   const peso = document.getElementById('peso').value;
   const resultado = document.getElementById('resultado');
+  var idade = document.getElementById('idade');
 
-  if (altura!=='' && peso!==''){
-    
-    const valorIMC = (peso / (altura * altura)).toFixed(2);
-
-    let classificacao = '';
-    if(valorIMC < 10){
-      classificacao = 'desnutrição grau V';
-    }else if(valorIMC < 12.9){
-      classificacao = 'desnutrição grau IV';
-    }else if(valorIMC < 15.9){
-      classificacao = 'desnutrição grau III';
-    }else if(valorIMC < 16.9){
-      classificacao = 'desnutrição grau II';
-    }else if(valorIMC < 18.4){
-      classificacao = 'desnutrição grau I';
-    }else if(valorIMC < 24.9){
-      classificacao = 'normal';
-    }else if(valorIMC < 29.9){
-      classificacao = 'pré-obesidade';
-    }else if(valorIMC < 34.5){
-      classificacao = 'obesidade grau I';
-    }else if(valorIMC < 39.9){
-      classificacao = 'obesidade grau II';
-    }else if(valorIMC > 40){
-      classificacao = 'obesidade grau III';
-    }
-
-    resultado.innerHTML = `<p>O seu IMC é ${valorIMC} e o estado nutricional está classificado como ${classificacao}.</p>`
+  if (altura!=='' && peso!==''){ 
+    valorIMC = (peso / (altura * altura)).toFixed(2);
   }else{
-    resultado.innerHTML = '<p>Preencha os campos!</p>'
+    resultado.innerHTML = '<p>Preencha os campos!</p>';
   }
+
+  var msg, nivel = '';
+  if(sexo.value == 'masculino'){
+    nivel = imcMasculino(idade);
+    msg = `<p>O seu IMC é ${valorIMC} e o estado nutricional está classificado como ${nivel}.</p>`;
+  }else{
+    nivel = imcFeminino(idade);
+    msg = `<p>O seu IMC é ${valorIMC} e o estado nutricional está classificado como ${nivel}.</p>`;
+  }
+
+  resultado.innerHTML = msg;
+
 }
-
-submit.addEventListener('click', imc) 
-
